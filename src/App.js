8ed6aef54459ee4d08c4cd0f729dad1e84c0821c
Delbin <delbin2019@gmail.com>
2024-11-04 15:00:24 +0530

    route1

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import HotelRegister from './components/Hotel/HotelRegister';
 import UserRegister from './components/User/UserRegister';
+import  { Route, Routes,BrowserRouter } from 'react-router-dom';
+import UserLogin from './components/User/UserLogin';
 
 function App() {
   return (
+    <BrowserRouter>
    <div>
-<UserRegister/>
+    <Routes>
+      <Route path='/' element={<UserLogin/>}/>
+    </Routes>
+
    </div>
+   </BrowserRouter>
   
   );
 }
--- src/components/User/UserLogin.js
+++ src/components/User/UserLogin.js
@@ -2,7 +2,27 @@ import React from 'react'
 
 function UserLogin() {
   return (
-    <div>my file</div>
+    <div className="login-container">
+    <div className="right-panel">
+      <div className="form-container">
+        <h3>Login to Your Account</h3>
+        <form className='form'>
+          <div className="form-row">
+            <input type="text" placeholder="Username" required /> {/* Username field */}
+          </div>
+          <div className="form-row">
+            <input type="password" placeholder="Password" required />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+        <p>
+          Don't have an account? <a href="/signup">Sign up here!</a>
+        </p>
+        <footer>&copy; 2024. Company Inc. Ltd.</footer>
+      </div>
+    </div>
+  </div>
+  
   )
 }
 
