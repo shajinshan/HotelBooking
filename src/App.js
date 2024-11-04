578747cc7aa1209434f7338f605f286f34a3d017
shajinshan <shajinshajin02@gmail.com>
2024-11-04 13:09:29 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import HotelRegister from './components/Hotel/HotelRegister';
+import UserRegister from './components/User/UserRegister';
+
+function App() {
+  return (
+   <div>
+<UserRegister/>
+   </div>
+  
+  );
+}
+
+export default App;
---
+++ src/components/User/UserRegister.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import '/Users/shajinshan/Desktop/react/hotel-booking/src/components/User/usercss/UserRegistration.css'
+
+
+function UserRegister() {
+  return (
+    <div className="signup-container">
+    <div className="left-panel">
+      <h2>“</h2>
+      <h1>Let's Make it Happen Together!</h1>
+    </div>
+    <div className="right-panel">
+      <div className="form-container">
+      
+        <p>
+          Already have an account? <a href="/login">Login here!</a>
+        </p>
+        <h3>Create An Account</h3>
+        <form>
+          <div className="form-row">
+            <input type="text" placeholder="First Name" required />
+            <input type="text" placeholder="Last Name" required />
+          </div>
+          <div className="form-row">
+            <input type="email" placeholder="Email" required />
+            <input type="text" placeholder="Address" required />
+          </div>
+          <div className="form-row">
+            <input type="number" placeholder="Aadhar Number" required />
+            <input type="number" placeholder="Mobile" required />
+          </div>
+          <div className="form-row">
+            <input type="password" placeholder="Create Password" required />
+            <input type="password" placeholder="Confirm Password" required />
+          </div>
+          
+          <div className="terms">
+            <input type="checkbox" required />
+            <label>
+              Creating your account means you accept our{" "}
+              <a href="/terms">Terms & Conditions</a>.
+            </label>
+          </div>
+          <button type="submit">Create Account</button>
+         
+        </form>
+        <footer>&copy; 2024. Company Inc. Ltd.</footer>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default UserRegister
