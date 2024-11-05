a282fe0841afcdefed46be8f92e101521999ab18
shajinshan <shajinshajin02@gmail.com>
2024-11-05 10:20:44 +0530

    usercomplete

---
+++ src/components/hotel/HotelAdminRegister.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './hotelcss/HotelAdminRegister.css'
+function HotelAdminRegister() {
+  return (
+    <div className='h-admin-reg-bg'>
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default HotelAdminRegister
