760f541e9cc236e9264f78354edd3cd89d24ad54
shajinshan <shajinshajin02@gmail.com>
2024-11-06 12:57:40 +0530

    nw

--- src/components/Admin/AdminLogin.js
+++ src/components/Admin/AdminLogin.js
@@ -4,14 +4,11 @@ import './AdminCss/AdminLogin.css';
 
 function AdminLogin() {
   return (
-    <div>
+    <div className='admin-log-bg'>
 <div className="admin-login-container">
       <div className="admin-form-container">
         <h3>Admin Login</h3>
         <form className="admin-form">
-          <div className="admin-form-row">
-            <input type="text" placeholder="Username" required />
-          </div>
           <div className="admin-form-row">
             <input type="email" placeholder="Email" required />
           </div>
