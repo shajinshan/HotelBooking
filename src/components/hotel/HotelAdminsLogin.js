e485dfa573eec717269c73d70294b4572b1d92bb
Delbin <delbin2019@gmail.com>
2024-11-06 12:59:38 +0530

    dummy

--- src/components/hotel/HotelAdminsLogin.js
+++ src/components/hotel/HotelAdminsLogin.js
@@ -5,21 +5,9 @@ import React from 'react'
 function HotelAdminsLogin() {
   return (
     <div>
-    <div className="login-page">
-  <div className="login-container">
-    <p className="login-text">
-      Don’t have an account? <a href="/signup">Sign up here!</a>
-    </p>
-    <h2>Hotel Login</h2>
-    <form>
-      <input type="text" placeholder="Username" className="input-field" />
-      <input type="password" placeholder="Password" className="input-field" />
-      <button type="submit" className="login-button">Login</button>
-    </form>
-  </div>
-  </div>
-  
-  </div>
+      
+    </div>
+    
   )
 }
 
