8f985422c22474b009b6cf7cb04a7d40571e3690
shajinshan <shajinshajin02@gmail.com>
2024-11-09 10:53:22 +0530

    nw

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import HotelAdminsLogin from './components/hotel/HotelAdminsLogin';
 import AdminLogin from './components/Admin/AdminLogin';
 import AdminDashBoard from './components/Admin/AdminDashBoard';
+import PaymentComponent from './components/PaymentComponent';
 
 
 
@@ -30,6 +31,7 @@ function App() {
           <Route path='/hotelladminogin' element={<HotelAdminsLogin />} />
           <Route path='/adminlogin' element={<AdminLogin />} />
           <Route path='/admindashboard' element={<AdminDashBoard />} />
+          <Route path='/pay' element={<PaymentComponent/>}/>
 
         </Routes>
 
--- src/components/Admin/AdminDashBoard.js
+++ src/components/Admin/AdminDashBoard.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './AdminCss/AdminDashBoard.css';
 import Navbar from './Navbar';
+import axios from 'axios';
 
 function AdminDashBoard() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8081/admin/dashboard')
+      .then((res) => {
+        setData(res.data)
+        console.log(data);
+
+      })
+      .catch((err) => {
+        alert(err)
+      })
+  }, []);
+
+  //decline
+
+  function declineRequest(id){
+    axios.delete(`http://localhost:8081/admin/decline/${id}`)
+    .then((response) => {
+     
+      setData(data.filter((item) => item.id !== id));
+    })
+    .catch((err) => {
+     alert(err)
+    });
+  }
   return (
     <div className='a-dashboard'>
-      <Navbar/>
+      <Navbar />
       <div className='a-dash-container container'>
         <h1 className="text-center mb-4">Admin Dashboard</h1>
         <table className='table  table-hover table-bordered'>
@@ -19,22 +47,31 @@ function AdminDashBoard() {
               <th>Address</th>
               <th>Approve</th>
               <th>Decline</th>
-              
+
             </tr>
           </thead>
-          <tbody>
-            <tr>
-              <td>002</td>
-              <td>Shajin</td>
-              <td>sha@gmail.com</td>
-              <td>123-456-7890</td>
-              <td>Reg-001</td>
-              <td>1234 Elm Street</td>
-              <td> <button className='btn btn-success'> Accept</button></td>
-              <td > <button className='btn btn-danger'> decline</button></td>
-            </tr>
-           
-          </tbody>
+          {data.map((e) => {
+            return (
+              <tbody>
+
+
+                <tr>
+                  <td>{e.id}</td>
+                  <td>{e.organiserName}</td>
+                  <td>{e.email}</td>
+                  <td>{e.phone}</td>
+                  <td>{e.registerId}</td>
+                  <td>{e.address}</td>
+                  <td> <button className='btn btn-success'> Accept</button></td>
+                  <td > <button className='btn btn-danger' onClick={()=> declineRequest(e.id)}> decline</button></td>
+                </tr>
+
+
+
+
+              </tbody>
+            )
+          })}
         </table>
       </div>
     </div>
